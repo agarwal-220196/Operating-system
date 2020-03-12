d5f850a1c1bebe006441aa3647e6567f6f611306
agarwal-220196 <agarwal.220196@tamu.edu>
2020-03-12 07:17:44 -0500

    Testing VM pool with size 1 100 and size 2 100

--- MP4/MP4_Sources/kernel.C
+++ MP4/MP4_Sources/kernel.C
@@ -31,7 +31,7 @@
 
 #define FAULT_ADDR (4 MB)
 /* used in the code later as address referenced to cause page faults. */
-#define NACCESS ((26 MB) / 4)
+#define NACCESS ((1 MB) / 4)
 /* NACCESS integer access (i.e. 4 bytes in each access) are made starting at address FAULT_ADDR */
 
 /*--------------------------------------------------------------------------*/
@@ -216,7 +216,7 @@ int main() {
     Console::puts("Hello World!\n");
 
     /* Comment out the following line to test the VM Pools */
-#define _TEST_PAGE_TABLE_
+//#define _TEST_PAGE_TABLE_
 
 #ifdef _TEST_PAGE_TABLE_
 
@@ -242,9 +242,9 @@ int main() {
     Console::puts("of the VM Pool memory allocator.\n");
     Console::puts("Please be patient...\n");
     Console::puts("Testing the memory allocation on code_pool...\n");
-    GenerateVMPoolMemoryReferences(&code_pool, 50, 100);
+    GenerateVMPoolMemoryReferences(&code_pool, 100, 100);
     Console::puts("Testing the memory allocation on heap_pool...\n");
-    GenerateVMPoolMemoryReferences(&heap_pool, 50, 100);
+    GenerateVMPoolMemoryReferences(&heap_pool, 100, 100);
 
 #endif
 
