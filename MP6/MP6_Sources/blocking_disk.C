2bd526eb88d08a78af2fe05e865ea72341100391
agarwal-220196 <agarwal.220196@tamu.edu>
2020-04-15 02:52:45 -0500

    functions of blocking_disk written

--- MP6/MP6_Sources/blocking_disk.C
+++ MP6/MP6_Sources/blocking_disk.C
@@ -1,8 +1,8 @@
 /*
      File        : blocking_disk.c
 
-     Author      : 
-     Modified    : 
+     Author      : Sanket Vinod Agarwal 
+     Modified    : April, 15, 2020
 
      Description : 
 
@@ -22,17 +22,40 @@
 #include "utils.H"
 #include "console.H"
 #include "blocking_disk.H"
+#include "scheduler.H"
+#include "thread.H"
 
+extern Scheduler *SYSTEM_SCHEDULER // will be used to call different functions of scheduler class
 /*--------------------------------------------------------------------------*/
 /* CONSTRUCTOR */
 /*--------------------------------------------------------------------------*/
 
 BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size) 
   : SimpleDisk(_disk_id, _size) {
+	disk_queue_size = 0; //initialize to zero 
+	this->disk_queue = new round_robin_queue();
+
 }
 
-/*--------------------------------------------------------------------------*/
-/* SIMPLE_DISK FUNCTIONS */
+void BlockingDisk::wait_until_ready(){
+	if (!SimpleDisk::is_ready()){//if not ready then yield the CPU 
+		Thread *thread_current = Thread::CurrentThread();//get the current thread
+		this->disk_enqueue(thread_current);//add to the queue
+		SYSTEM_SCHEDULER->yield();// yield the cpu 
+	}
+
+}
+
+void BlockingDisk::disk_enqueue(Thread *thread){
+	this->disk_queue->enqueue_thread(thread);// do the queue addition task
+	disk_queue_size++;// increase the size of queue
+}
+
+bool BlockingDisk::is_ready(){// this function will be called from the scheduler to check if the thread has completed its I/O. That is the periodic check. 
+	return SimpleDisk::is_ready();
+}
+/*------------------------------------------------------------------------------
+	SIMPLE_DISK FUNCTIONS not adding the functionalities here but modifying the wait until ready function. */
 /*--------------------------------------------------------------------------*/
 
 void BlockingDisk::read(unsigned long _block_no, unsigned char * _buf) {
