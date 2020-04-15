84cc0adf5013c64a793b72e0a2899ac3c90f569a
agarwal-220196 <agarwal.220196@tamu.edu>
2020-04-14 21:57:44 -0500

    adding comments in console and bochsrc to enable terminal printing, same as MP5

--- MP6/MP6_Sources/console.C
+++ MP6/MP6_Sources/console.C
@@ -145,6 +145,7 @@ void Console::putch(const char _c){
     else if(_c == '\r')
     {
         csr_x = 0;
+	Machine::outportb(0xe9, '\r'); // same as MP5
     }
     /* We handle our newlines the way DOS and the BIOS do: we
     *  treat it as if a 'CR' was also there, so we bring the
@@ -153,6 +154,7 @@ void Console::putch(const char _c){
     {
         csr_x = 0;
         csr_y++;
+	Machine::outportb(0xe9, '\n'); // same as MP5
     }
     /* Any character greater than and including a space, is a
     *  printable character. The equation for finding the index
@@ -163,6 +165,7 @@ void Console::putch(const char _c){
         unsigned short * where = textmemptr + (csr_y * 80 + csr_x);
         *where = _c | (attrib << 8);	/* Character AND attributes: color */
         csr_x++;
+	Machine::outportb(0xe9, _c); // same as MP5
     }
 
     /* If the cursor has reached the edge of the screen's width, we
